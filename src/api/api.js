fe2dd11a57aeec45a9852216f9b37f599cc6225d
Miguel Medellín <mamedellinr@unal.edu.co>
2022-02-07 07:36:18 -0500

    StartUp Project

---
+++ src/api/api.js
@@ -0,0 +1,20 @@
+// import { ApiUrl } from '../config/default';
+import axios from 'axios';
+
+const API_URL = "https://cg-backend-api.herokuapp.com";
+
+export async function getProducts() {
+    var products = [];
+    const res = await axios.get(API_URL + '/api/v1/products/all');
+    for (const product of res.data.data) {
+        var { data } = await getProduct(product._id);
+        data.id = product._id;
+        products.push(data)
+    }
+    return products;
+}
+
+export async function getProduct(id) {
+    const res = await axios.get(API_URL + '/api/v1/products/' + id);
+    return res.data;
+}
---
+++ src/components/Products/ProductCard.js
@@ -0,0 +1,32 @@
+const ProductCard = (props) => {
+    var product = props.product;
+    return (
+        <div class="col-12 col-md-4 mb-4">
+            <div class="card h-100 rounded-lg">
+                <a href="#">
+                    <img class="rounded-top-lg w-100" src={product.photoUrl || 'invalid url'} alt="Drone image" />
+                </a>
+                <div class="card-body">
+                    <ul class="list-unstyled d-flex justify-content-between">
+                        <li>
+                            <i class="text-warning fa fa-star"></i>
+                            <i class="text-warning fa fa-star"></i>
+                            <i class="text-warning fa fa-star"></i>
+                            <i class="text-muted fa fa-star"></i>
+                            <i class="text-muted fa fa-star"></i>
+                        </li>
+                        <li class="text-muted text-right">{product.price || 'Drone price'}</li>
+                    </ul>
+                    <a href="#" class="h2 text-decoration-none text-dark">{product.name || 'Drone name'}</a>
+                    <p class="card-text">
+                        {product.description || 'Drone description'}
+                    </p>
+                    <p class="text-muted">Stock ({product.stock || 0})</p>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default ProductCard;
